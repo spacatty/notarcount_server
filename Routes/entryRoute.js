a9bf28c70b685dda1eb7c1869a9903ee18d56dd9
anxietyangst <bezdnastada@gmail.com>
2020-05-04 21:00:16 +0300

    API /NORMALIZED WEEKS IMPROVEMENTS, ROUTE IS STABLE

--- Routes/entryRoute.js
+++ Routes/entryRoute.js
@@ -2,6 +2,24 @@ const express = require("express");
 const entrySchema = require("../Schemas/entrySchema");
 const entryRoute = express.Router();
 
+const countWeek = (week) => {
+  let weekIncome = 0;
+  let weekExpense = 0;
+  let weekRevenue = 0;
+  week.forEach((day) => {
+    weekIncome += day.entryData.totalData[0].totalIncome;
+    weekExpense += day.entryData.totalData[0].totalExpense;
+    weekRevenue += day.entryData.totalData[0].totalCount;
+  });
+
+  const resultingObject = {
+    weekIncome,
+    weekExpense,
+    weekRevenue,
+  };
+  return resultingObject;
+};
+
 entryRoute.get("/", async (req, res) => {
   try {
     const everythingFound = await entrySchema.find({});
@@ -10,6 +28,7 @@ entryRoute.get("/", async (req, res) => {
     res.status(501).send({ error });
   }
 });
+
 entryRoute.get("/normalized", async (req, res) => {
   let arrayOfEntries = [];
   let arrayOfWeeks = [];
@@ -24,10 +43,16 @@ entryRoute.get("/normalized", async (req, res) => {
 
   class week {
     constructor(slicedArray) {
-      this.finished = slicedArray.length == 5 ? true : false
-      this.periodFrom = slicedArray[0].createdAt
-      this.periodTill = this.finished ? slicedArray[slicedArray.length - 1].createdAt : "../../...."
-      this.weekEntryData = slicedArray
+      const weekCountingData = countWeek(slicedArray)
+      this.finished = slicedArray.length == 5 ? true : false;
+      this.periodFrom = slicedArray[0].createdAt;
+      this.periodTill = this.finished
+        ? slicedArray[slicedArray.length - 1].createdAt
+        : "../../....";
+      this.weekEntryData = slicedArray;
+      this.weekIncome = weekCountingData.weekIncome
+      this.weekExpense = weekCountingData.weekExpense
+      this.weekRevenue = weekCountingData.weekRevenue
     }
   }
 
@@ -37,10 +62,10 @@ entryRoute.get("/normalized", async (req, res) => {
       arrayOfEntries.push(new entry(aEntry));
     });
     for (let i = 0, l = arrayOfEntries.length; i < l; i += 5) {
-      arrayOfWeeks.push(new week(arrayOfEntries.slice(i, i + 5)))
+      arrayOfWeeks.push(new week(arrayOfEntries.slice(i, i + 5)));
     }
     res.status(200).send({
-      arrayOfWeeks
+      arrayOfWeeks,
     });
   } catch (error) {
     res.status(501).send({ error });
